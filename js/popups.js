c0aac4946680a3c2c55baf8ccfa7c1f1caff8086
Dima <damios.1812@gmail.com>
2022-08-26 10:07:13 +0300

    first commit

---
+++ js/popups.js
@@ -0,0 +1,23 @@
+let popupsToggle = document.querySelectorAll('.open-popup');
+let popupClose = document.querySelectorAll('.close');
+
+popupsToggle.forEach(function(item){
+    item.addEventListener('click',function(){
+        let popupName = item.getAttribute('data-popup');
+        document.getElementById(popupName).style.display = "block";
+    })
+})
+
+popupClose.forEach(function(item){
+    item.addEventListener('click',function(){
+        let popup = item.closest('.popup');
+        popup.style.display="none";
+    })
+})
+
+window.onclick = function(e) {
+    if(e.target.classList.contains('popup')) {
+        e.target.style.display = "none";
+    }
+}
+
